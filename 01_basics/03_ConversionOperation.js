57905110a379fbf6766e39a9805b287bc925c2cc
keerthanvedha-sys <keerthanvedha@gmail.com>
2025-10-18 16:06:48 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof score);
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+// "33"=>33
+// "33abc"=> NaN
+//true=>1; false=> 0
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true; 0=>false
+// ""=> false
+// "hitesh"=> true
+let someNumber =74
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
