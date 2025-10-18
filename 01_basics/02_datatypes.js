44ae47711a3b953a23d5e2230eac564545a626b5
keerthanvedha-sys <keerthanvedha@gmail.com>
2025-10-18 15:33:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";
+//alert(3+3) we are using nodejs not browser
+let name="Keerthan" //string
+let age = 18
+let isloggedIn= false
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+//undefined
+// symbol => unique
+// object
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
