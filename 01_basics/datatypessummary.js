6852b54a8ea1a3ad886336a6c1d4fbb6592a2ad8
keerthanvedha-sys <keerthanvedha@gmail.com>
2025-10-20 11:06:31 +0000

    datatypesummsry

---
+++ 01_basics/datatypessummary.js
@@ -0,0 +1,27 @@
+//primitive 
+// 7types string, Number, Boolean, null, undefined, Symbol, BigInt
+let useremail=null;
+console.log(useremail);
+
+// reference type(non primitive) array,Objects,functions
+//array
+const heros =["shaktiman","ironman"];
+//obj
+let myobj={
+    name : "keerthan",
+    age : 22,
+}
+//function
+const myFunction =function(){
+    console.log("hello world")
+}
+/*
+1) The data type of null is an object.
+2) The data type of String is a string.
+3) The data type of Boolean is Boolean.
+4) The data type of Undefined is undefined.
+5) The data type of Symbol is a symbol.
+6) The data type of Number is number.
+7) The data type of BigInt is BigInt.   
+  And the Data Types of Non primitive data type is Function object
+*/
